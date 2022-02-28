d66d927535c992c9d7e7365cf27d0f18fc8a6b28
Karol Tarasiuk <karol.tarasiuk@ocuco.com>
2022-02-28 15:58:02 +0100

    Added sample

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Ubuntu Regular";
+  src: local("Ubuntu Regular"), url(./fonts/Ubuntu-L.ttf) format("truetype");
+  color: #d8e1e7;
+  background: #36454f;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
